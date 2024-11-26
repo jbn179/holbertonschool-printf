d76e5334f89c788e536cf57ce8dbdc2af8b55019
ni-mdj <9855@holbertonstudents.com>
2024-11-26 15:00:28 +0100

    add typedef struct

--- _printf.c
+++ _printf.c
@@ -1,39 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: a character string containing zero or more directives
- * Description: produces output according to a format
- * Return: the number of characters printed
+ * _printf - Produces output according to a format
+ * @format: A character string containing zero or more directives
+ *
+ * Return: The number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+va_list args;
+int count;
+int i;
+char specifier;
 
-	va_start(args, format);
+format_specifier specifiers[] = {
+{'c', print_char},
+{'s', print_string},
+{'d', print_integer},
+{'i', print_integer},
+    {'%', print_char}
+    };
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-				count += print_char(va_arg(args, int));
-			else if (*format == 's')
-				count += print_string(va_arg(args, char *));
-			else if (*format == 'd' || *format == 'i')
-				count += print_integer(va_arg(args, int));
-			else if (*format == '%')
-				count += print_char('%');
-		}
-		else
-		{
-			count += print_char(*format);
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+    va_start(args, format);
+
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            specifier = format[i];
+            for (int j = 0; j < 5; j++)
+            {
+                if (specifier == specifiers[j].specifier)
+                {
+                    count += specifiers[j].func(args);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            count += print_char(format[i]);
+        }
+        i++;
+    }
+
+    va_end(args);
+
+    return count;
 }
