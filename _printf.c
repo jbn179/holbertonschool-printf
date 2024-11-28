03b9d4bde844423b660bc2b4de9703f25409ff7c
jbn179 <9772@holbertonstudents.com>
2024-11-28 10:02:21 +0100

    add comments to printf.c

--- _printf.c
+++ _printf.c
@@ -4,7 +4,13 @@
 /**
  * _printf - Produces output according to a format
  * @format: A character string containing zero or more directives
- *
+ * Description : This function processes a format string and prints the
+ * corresponding characters or values based on the specifiers it
+ * encounters.
+ * The function use va_list to handle variable arguments passed
+ * after the format string, and an array of format_specifier
+ * structures to map each specifier ('c', 's', 'd', 'i', '%')
+ * to its associated printing function.
  * Return: The number of characters printed
  */
 
